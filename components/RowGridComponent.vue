41d30182ed35bccb23676ec9c67a09dcaf181ec4
Francesco Mazzola <francescomazzola11@gmail.com>
2022-05-26 14:43:41 +0200

    fixing kri's mess

--- components/RowGridComponent.vue
+++ components/RowGridComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <span>
-    <hr />
     <div class="row row-cols-1 g-4 mt-4 m-lg-auto pt-3 w-75 ">
       <!-- v-for in the col element to display all the events fetched -->
       <div
