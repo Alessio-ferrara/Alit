b3a36cbd2c09069ae552126caced9d0a93df6d64
Alessio-ferrara <66694308+Alessio-ferrara@users.noreply.github.com>
2022-05-29 22:25:03 +0200

    revert change bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [{ src: "~/assets/bootstrap/mystyle.scss", lang: "sass" }],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["@/plugins/truncate.js"],
