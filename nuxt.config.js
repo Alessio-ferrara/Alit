09c7e2b3b6c63d55d48a04c21d7f1db0f36fb28c
Alessio-ferrara <66694308+Alessio-ferrara@users.noreply.github.com>
2022-05-29 22:34:21 +0200

    fix production error

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,4 +88,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {
+      common: {
+        name: "common",
+        chunks: "all",
+      },
+    };
+  },
 };
