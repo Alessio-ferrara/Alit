515ecf4127052b60155849b61af1ad22adec45b3
ermenda <ermendahoxha@outlook.com>
2022-06-02 16:32:49 +0200

    contact page

--- .nuxt/nuxt-google-maps.js
+++ .nuxt/nuxt-google-maps.js
@@ -1,15 +1,8 @@
 import Vue from 'vue';
-<<<<<<< HEAD
 import GMap from "C:\\Users\\Ermenda Hoxha\\Desktop\\Alit\\node_modules\\nuxt-gmaps\\components\\GMap.vue";
 import GMapMarker from "C:\\Users\\Ermenda Hoxha\\Desktop\\Alit\\node_modules\\nuxt-gmaps\\components\\GMapMarker.vue";
 import GMapCircle from "C:\\Users\\Ermenda Hoxha\\Desktop\\Alit\\node_modules\\nuxt-gmaps\\components\\GMapCircle.vue";
 import GMapInfoWindow from "C:\\Users\\Ermenda Hoxha\\Desktop\\Alit\\node_modules\\nuxt-gmaps\\components\\GMapInfoWindow.vue";
-=======
-import GMap from "\u002FUsers\u002Ffrancesco\u002Fgit\u002FAlit\u002Fnode_modules\u002Fnuxt-gmaps\u002Fcomponents\u002FGMap.vue";
-import GMapMarker from "\u002FUsers\u002Ffrancesco\u002Fgit\u002FAlit\u002Fnode_modules\u002Fnuxt-gmaps\u002Fcomponents\u002FGMapMarker.vue";
-import GMapCircle from "\u002FUsers\u002Ffrancesco\u002Fgit\u002FAlit\u002Fnode_modules\u002Fnuxt-gmaps\u002Fcomponents\u002FGMapCircle.vue";
-import GMapInfoWindow from "\u002FUsers\u002Ffrancesco\u002Fgit\u002FAlit\u002Fnode_modules\u002Fnuxt-gmaps\u002Fcomponents\u002FGMapInfoWindow.vue";
->>>>>>> 088047ccb237daf92ecb33a8ed5565b90e81b377
 export default(context, inject) =>{
   Vue.component('GMap', GMap);
   Vue.component('GMapMarker', GMapMarker);
--- pages/contact_us.vue
+++ pages/contact_us.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="container-fluid w-50 mt-5 pt-5">
-      <h4 class="lead text-center text-black">{{ contact.title }}</h4>
+    <h4 class="lead text-center text-black">{{ contact.title }}</h4>
       
-        <!-- <contact-form></contact-form> -->
+    <contact-form></contact-form>
   </div>
 </template>
 
 
 <script>
-// import ContactForm from '~/components/ContactForm.vue'
-// import '../../assets/style.css';
-// import '../../assets/details.css';
+import ContactForm from '~/components/ContactForm.vue'
 
 export default {
     name: "ContactUsPage",
-    // components: {
-    //     ContactForm,
-    // },
+    components: {
+        ContactForm,
+    },
     async asyncData({ route, $axios }) {
         const { data } = await $axios.get(`api/content/contact_us`);
         return {
